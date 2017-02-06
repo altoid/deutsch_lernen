9e0562b135809981656f5987908f5f200c36bb87
Doug Tomm <dtomm@radiumone.com>
2017-02-05 21:50:45 -0800

    handle verbs that end with -n but not -en

--- sql/verbs_noprefix.sql
+++ sql/verbs_noprefix.sql
@@ -5,6 +5,36 @@ word_id,
 word,
 attrvalue as 2nd_person_sing,
 
+substring(word, 1, char_length(word) - 1) inf_stem,
+case
+when attrvalue like '%est' then substring(attrvalue, 1, char_length(attrvalue) - 3)
+when attrvalue like '%st'  then substring(attrvalue, 1, char_length(attrvalue) - 2)
+when attrvalue like '%ßt'  then substring(attrvalue, 1, char_length(attrvalue) - 1)
+else NULL
+end as attrstem,
+
+
+substring(word, 1, char_length(word) - 1) <>
+case
+when attrvalue like '%est' then substring(attrvalue, 1, char_length(attrvalue) - 3)
+when attrvalue like '%st'  then substring(attrvalue, 1, char_length(attrvalue) - 2)
+when attrvalue like '%ßt'  then substring(attrvalue, 1, char_length(attrvalue) - 1)
+else NULL
+end as irregular
+
+from mashup
+where attrkey = 'second_person_singular'
+and pos_name = 'verb'
+and not attrvalue regexp ' '
+and word not like '%en'
+
+union
+
+select
+word_id,
+word,
+attrvalue as 2nd_person_sing,
+
 substring(word, 1, char_length(word) - 2) inf_stem,
 case
 when attrvalue like '%est' then substring(attrvalue, 1, char_length(attrvalue) - 3)
@@ -20,13 +50,8 @@ when attrvalue like '%est' then substring(attrvalue, 1, char_length(attrvalue) -
 when attrvalue like '%st'  then substring(attrvalue, 1, char_length(attrvalue) - 2)
 when attrvalue like '%ßt'  then substring(attrvalue, 1, char_length(attrvalue) - 1)
 else NULL
-end as irregular,
+end as irregular
 
--- attrvalue like '%est',
--- attrvalue like '%st',
--- attrvalue like '%ßt',
--- attrvalue like '%sst',
-1
 from mashup
 where attrkey = 'second_person_singular'
 and pos_name = 'verb'
