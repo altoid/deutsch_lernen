9e46cd6d955f08d3213ec2a21fc442920ad21c87
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-08-13 13:57:17 -0700

    rewrote quiz query to use new quiz_v view.

--- dlernen/quiz_sql.py
+++ dlernen/quiz_sql.py
@@ -21,184 +21,109 @@
 #
 # scores for every quiz/word/attribute, plus presentation time.
 #
-# quiz_attr_count
-# NFI
-#
-# quiz_word_attr_count
-# NFI
-#
 # how we select what to test:
 #
 # - crappiest score
-# - never tested
 # - too few attempts
 # - been a long time
 #
 # for a score over 95%, don't retest.  unless it's been more than 30 days.
 
-join_clauses = [
-    "inner join quiz_structure qstruct on q.id = qstruct.quiz_id",
-    "inner join word w on qstruct.pos_id = w.pos_id",
-    "inner join word_attribute wa on w.id = wa.word_id and wa.attribute_id = qstruct.attribute_id",
-    """left join quiz_score qscore on
-             w.id = qscore.word_id
-             and qstruct.quiz_id = qscore.quiz_id
-             and qstruct.attribute_id = qscore.attribute_id"""
-]
+QUIZ_SQL = """
+with attrs_for_quiz as (
+select qs.quiz_id, qs.attribute_id
+from quiz
+inner join quiz_structure qs on quiz.id = qs.quiz_id
+where quizkey = '{quizkey}'
+),
+given_words as (
+select id word_id from word
+where id in ({word_ids})
+),
+testable_attributes as (
+select * from
+attrs_for_quiz,
+given_words
+),
+testable_attrs_and_values as (
+select ta.*,
+mashup_v.word, mashup_v.attrvalue
+from testable_attributes ta
+inner join mashup_v on ta.word_id = mashup_v.word_id and ta.attribute_id = mashup_v.attribute_id
+)
 
-never_tested_filter = ["""qscore.word_id is null"""]
-crappy_score_filter = [
-    """presentation_count > 5""",
-    """(correct_count / presentation_count) <= 0.80"""]
+-- word has been presented 5 or fewer times (or not at all)
 
-too_few_attempts_filter = [
-    """presentation_count <= 5"""]
+select
+ta.quiz_id,
+ta.attribute_id,
+ta.word_id,
+ta.word,
+ta.attrvalue,
+ifnull(v.presentation_count, 0) presentation_count,
+ifnull(v.correct_count, 0) correct_count,
+v.last_presentation
+from testable_attrs_and_values ta
+left join quiz_v v
+on ta.word_id = v.word_id
+and ta.quiz_id = v.quiz_id
+and ta.attribute_id = v.attribute_id
 
-been_too_long_filter = ["""CURDATE() - INTERVAL 30 DAY > last_presentation"""]
-
-next_item_query = """
--- need quiz_id, word_id, attribute_id from other tables.  these are the PK of quiz_score.
-
-
-        -- never tested
-select 
-       word, value, quiz_id, word_id, attribute_id,
-       ifnull(presentation_count, 0) presentation_count,
-       ifnull(correct_count, 0) correct_count,
-       'never_tested' method
-from
-        (
-        select
-                w.word,
-                wa.value, 
-                q.id quiz_id,
-                w.id word_id,
-                wa.attribute_id,
-                qscore.presentation_count,
-                qscore.correct_count
-        from quiz q
-        {join_clauses}
-        where
-                {never_tested_filter}
-        order by rand()
-        limit 2
-        ) never_tested
+where ifnull(presentation_count, 0) <= 5
 
 union
 
--- crappy score (for words presented >= 10 times)
-select 
-       word, value, quiz_id, word_id, attribute_id,
-       ifnull(presentation_count, 0) presentation_count,
-       ifnull(correct_count, 0) correct_count,
-       'crappy_score' method
-from
-        (       
-        select
-                w.word,
-                wa.value,
-                q.id quiz_id,
-                w.id word_id,
-                wa.attribute_id,
-                qscore.presentation_count,
-                qscore.correct_count
-        from quiz q
-        {join_clauses}
-        where
-              {crappy_score_filter}
-        order by
-              presentation_count,
-              (correct_count / presentation_count)
-        limit 2
-        ) crappy_score
+-- crappy score (<= 80% in 10 or more presentations
+
+select
+ta.quiz_id,
+ta.attribute_id,
+ta.word_id,
+ta.word,
+ta.attrvalue,
+ifnull(v.presentation_count, 0) presentation_count,
+ifnull(v.correct_count, 0) correct_count,
+v.last_presentation
+from testable_attrs_and_values ta
+inner join quiz_v v
+on ta.word_id = v.word_id
+and ta.quiz_id = v.quiz_id
+and ta.attribute_id = v.attribute_id
+
+where presentation_count >= 10
+and correct_count / presentation_count <= 0.80
 
 union
 
--- too few attempts (<= 5)
-select 
-       word, value, quiz_id, word_id, attribute_id,
-       ifnull(presentation_count, 0) presentation_count,
-       ifnull(correct_count, 0) correct_count,
-       'too_few_attempts' method
-from
-        (       
-        select
-                w.word,
-                wa.value,
-                q.id quiz_id,
-                w.id word_id,
-                wa.attribute_id,
-                qscore.presentation_count,
-                qscore.correct_count
-        from quiz q
-        {join_clauses}
-        where
-              {too_few_attempts_filter}
-        order by
-              presentation_count,
-              (correct_count / presentation_count)
-        limit 2
-        ) too_few_attempts
+-- word hasn't been quizzed in more than 30 days
 
-union
+select
+ta.quiz_id,
+ta.attribute_id,
+ta.word_id,
+ta.word,
+ta.attrvalue,
+ifnull(v.presentation_count, 0) presentation_count,
+ifnull(v.correct_count, 0) correct_count,
+v.last_presentation
+from testable_attrs_and_values ta
+inner join quiz_v v
+on ta.word_id = v.word_id
+and ta.quiz_id = v.quiz_id
+and ta.attribute_id = v.attribute_id
 
--- too much time since last trial (30 days)
-select 
-       word, value, quiz_id, word_id, attribute_id,
-       ifnull(presentation_count, 0) presentation_count,
-       ifnull(correct_count, 0) correct_count,
-       'been_too_long' method
-from
-        (
-        select
-                w.word,
-                wa.value,
-                q.id quiz_id,
-                w.id word_id,
-                wa.attribute_id,
-                qscore.presentation_count,
-                qscore.correct_count
-        from quiz q
-        {join_clauses}
-        where
-              {been_too_long_filter}
-        order by
-              last_presentation
-        limit 2
-        ) been_too_long
+where curdate() - interval 30 day > last_presentation
 
-order by rand()
-limit 1
 """
 
 
-def build_quiz_query(quizkey, **kwargs):
-    global never_tested_filter
-    global crappy_score_filter
-    global been_too_long_filter
-    global too_few_attempts_filter
-
-    join_clauses_arg = kwargs.get('join_clauses', [])
-    where_clauses = kwargs.get('where_clauses', [])
-
-    j_clauses = join_clauses + join_clauses_arg
-    j_clauses = ' '.join(j_clauses)
-    d1 = {
-        'quizkey': quizkey,
+def build_quiz_query(quizkey, word_ids):
+    """
+    word_ids should be a string that is a comma-separated list of word_ids
+    """
+    d = {
+        "quizkey": quizkey,
+        "word_ids": word_ids
     }
 
-    qkey = "q.quizkey = '{quizkey}'".format(**d1)
-    never_tested_filter += [qkey] + where_clauses
-    crappy_score_filter += [qkey] + where_clauses
-    too_few_attempts_filter += [qkey] + where_clauses
-    been_too_long_filter += [qkey] + where_clauses
-
-    d2 = {
-        'join_clauses': j_clauses,
-        'never_tested_filter': ' AND '.join(never_tested_filter),
-        'crappy_score_filter': ' AND '.join(crappy_score_filter),
-        'been_too_long_filter': ' AND '.join(been_too_long_filter),
-        'too_few_attempts_filter': ' AND '.join(too_few_attempts_filter),
-    }
-
-    return next_item_query.format(**d2)
+    return QUIZ_SQL.format(**d)
