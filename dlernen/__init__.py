324b33e3d014562e84131d5a7ba3750fc2a5b867
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2025-10-31 21:18:04 -0700

    changed import statement for blueprint

--- dlernen/__init__.py
+++ dlernen/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from dlernen import config
-from dlernen.dlernen import bp
+from dlernen.dlernen import bp as dlernen_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "ap.i*&(^ap1."
     app.config.from_object(config.Config)
-    app.register_blueprint(bp)
+    app.register_blueprint(dlernen_bp)
 
     return app
