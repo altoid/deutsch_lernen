26df4ad9f7fc409d1b2177575b02f99ff1dda9f9
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2025-11-15 20:26:48 -0800

    in get_pos, take an optional word and enrich the pos structure with all its attributes.

--- dlernen/api_misc.py
+++ dlernen/api_misc.py
@@ -37,11 +37,44 @@ def gender_rules():
 def get_pos():
     # TODO - make sure the word conforms to dlernen_json_schema.WORD_PATTERN
     word = request.args.get('word')
-    
-    """
-    fetch the part-of-speech info from the database and format it
-    """
-    with closing(connect(**current_app.config['DSN'])) as dbh, closing(dbh.cursor(dictionary=True)) as cursor:
+
+    if word:
+        sql = """
+        WITH pos_data
+             AS (SELECT p.NAME pos_name,
+                        p.id   pos_id,
+                        a.attrkey,
+                        a.id   AS attribute_id,
+                        pf.sort_order
+                 FROM   pos_form pf
+                        INNER JOIN pos p
+                                ON p.id = pf.pos_id
+                        INNER JOIN attribute a
+                                ON a.id = pf.attribute_id),
+             word_data
+             AS (SELECT word_id,
+                        word,
+                        attribute_id,
+                        attrvalue,
+                        attrvalue_id,
+                        pos_id
+                 FROM   mashup_v
+                 WHERE  word = %(word)s)
+        SELECT pd.pos_name,
+               pd.pos_id,
+               pd.attrkey,
+               pd.attribute_id,
+               pd.sort_order,
+               wd.word,
+               wd.word_id,
+               wd.attrvalue,
+               wd.attrvalue_id
+        FROM   pos_data pd
+               LEFT JOIN word_data AS wd
+                      ON pd.attribute_id = wd.attribute_id
+                         AND pd.pos_id = wd.pos_id 
+        """
+    else:
         sql = """
         SELECT p.NAME AS pos_name,
                p.id   AS pos_id,
@@ -59,7 +92,15 @@ def get_pos():
                        ON a.id = pf.attribute_id 
         """
 
-        cursor.execute(sql)
+    """
+    fetch the part-of-speech info from the database and format it
+    """
+    with closing(connect(**current_app.config['DSN'])) as dbh, closing(dbh.cursor(dictionary=True)) as cursor:
+        if word:
+            cursor.execute(sql, {'word': word})
+        else:
+            cursor.execute(sql)
+            
         rows = cursor.fetchall()
 
         name_to_attrs = {}
