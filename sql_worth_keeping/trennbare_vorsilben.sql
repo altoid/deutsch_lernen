20899fe31ba3d94c7304819403324c59a42e12ee
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2026-04-29 14:31:43 -0700

    the query give a trio of word ids for the prefix, the root, and the whole verb.

--- sql_worth_keeping/trennbare_vorsilben.sql
+++ sql_worth_keeping/trennbare_vorsilben.sql
@@ -9,23 +9,29 @@ SUBSTRING_INDEX(attrvalue, ' ', -1) prefix, word_id, word,
 from mashup_v
 where attrkey = 'second_person_singular'
 and attrvalue like '% %'
-)
+),
+separable_prefix as (
+-- this will give the word_ids for the verb, the separable prefix, and the root, subject to the following conditions:
+--
+-- 1.  the second person singular for the verb is present
+-- 2.  the SPS shows that there is a separable prefix
+-- 3.  the prefix exists in the database
+-- 4.  the root exists in the database
+-- 
 select distinct
-    extracted_prefix.word_id,
-    extracted_prefix.word,
-    m1.word_id root_word_id,
-    m1.word root,
-    m2.word_id prefix_word_id,
-    m2.word prefix
+extracted_prefix.word_id, m1.word_id prefix_word_id, m2.word_id root_word_id
 from extracted_prefix
 
-inner join mashup_v m1 on extracted_prefix.root = m1.word
-inner join mashup_v m2 on extracted_prefix.prefix = m2.word
+inner join mashup_v m1
+on m1.word = extracted_prefix.prefix
 
-where m1.pos_name = 'verb'
-and m2.pos_name = 'separable prefix'
+inner join mashup_v m2
+on m2.word = extracted_prefix.root
 
-order by prefix, root
+where m1.pos_name = 'separable prefix'
+and m2.pos_name = 'verb'
+)
+select * from separable_prefix
 ;
 
 -- for every verb, gimme:
