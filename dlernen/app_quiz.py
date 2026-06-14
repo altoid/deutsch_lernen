34afc37b9e0899555f3e898e86865081087507e1
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2026-06-14 16:10:23 -0700

    trying to make this work; it doesn't, really

--- dlernen/app_quiz.py
+++ dlernen/app_quiz.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, url_for
-from pprint import pprint
+from pprint import pprint, pformat
 import requests
 import click
 import random
@@ -92,11 +92,17 @@ def quiz_words(wordlist_ids, queries):
 @bp.cli.command('quiz')
 @click.argument('quiz_key')
 @click.option('--wordlist_ids', '-l', multiple=True)
-@click.option('--query', '-q', multiple=True)
+@click.option('--queries', '-q', multiple=True)
 @click.option('--tags', '-t', multiple=True)
 def quiz(quiz_key, wordlist_ids, queries, tags):
+    print("quiz_key = |%s|, wordlist_ids = |%s|, queries = |%s|, tags = |%s|" % (
+        quiz_key,
+        pformat(wordlist_ids),
+        pformat(queries),
+        pformat(tags)
+    ))
     # to run this, use the command:
-    #   python -m flask --app run app_quiz_definitions quiz_definitions [-l id -l id -l id ...]
+    #   python -m flask --app run app_quiz quiz <quiz_key> [-l id -l id -l id ...]
     #
     # it has to be invoked from the dlernen directory.
     # need -l for each list id because click sucks but we can't use argparse.
@@ -108,7 +114,6 @@ def quiz(quiz_key, wordlist_ids, queries, tags):
             return "OK", 200
 
     counter = 0
-
     if len(wordlist_ids) > 1:
         url = url_for('api_quiz.get_word_to_test',
                       wordlist_id=wordlist_ids,
@@ -126,50 +131,54 @@ def quiz(quiz_key, wordlist_ids, queries, tags):
     while True:
         r = requests.get(url)
         if not r:
+            message = "%s [%s]" % (r.text, r.status_code)
+            print(message)
             return r.text, r.status_code
 
-        attr_to_test = r.json()
+        attrs_to_test = r.json()
 
-        if not attr_to_test:
+        if not attrs_to_test:
             print("es gibt keine Welten mehr zu erobern")
             break
 
-        counter += 1
-        if 'article' in attr_to_test:
-            word = "%s %s" % (attr_to_test['article'], attr_to_test['word'])
-        else:
-            word = "%s" % (attr_to_test['word'])
+        pprint(attrs_to_test)
+        for attr in attrs_to_test:
+            counter += 1
+            if 'article' in attr:
+                word = "%s %s" % (attr['article'], attr['word'])
+            else:
+                word = "%s" % (attr['word'])
 
-        print("############ %s ###########" % word)
-        prompt = "[%s] ===== %s ====> " % (counter, attr_to_test['attrkey'])
+            print("############ %s ###########" % word)
+            prompt = "[%s] ===== %s ====> " % (counter, attr['attrkey'])
 
-        answer = input(prompt).strip().casefold()
-        while not answer:
             answer = input(prompt).strip().casefold()
+            while not answer:
+                answer = input(prompt).strip().casefold()
 
-        if answer == 'q':
-            break
+            if answer == 'q':
+                break
 
-        correct = answer == attr_to_test['attrvalue'].casefold()
+            correct = answer == attr['attrvalue'].casefold()
 
-        if correct:
-            print("=======> richtig")
-        else:
-            print("######## falsch: %s" % attr_to_test['attrvalue'])
+            if correct:
+                print("=======> richtig")
+            else:
+                print("######## falsch: %s" % attr['attrvalue'])
 
-        payload = {
-            "quiz_id": attr_to_test['quiz_id'],
-            "word_id": attr_to_test['word_id'],
-            "attribute_id": attr_to_test['attribute_id'],
-            "correct": correct,
-        }
+            payload = {
+                "quiz_id": attr['quiz_id'],
+                "word_id": attr['word_id'],
+                "attribute_id": attr['attribute_id'],
+                "correct": correct,
+            }
 
-        r = requests.post(url_for('api_quiz.post_quiz_answer',
-                                  quiz_key=quiz_key,
-                                  _external=True), json=payload)
+            r = requests.post(url_for('api_quiz.post_quiz_answer',
+                                      quiz_key=quiz_key,
+                                      _external=True), json=payload)
 
-        if not r:
-            message = "could not post quiz answer:  %s [%s]" % (r.text, r.status_code)
-            return message, r.status_code
+            if not r:
+                message = "could not post quiz answer:  %s [%s]" % (r.text, r.status_code)
+                return message, r.status_code
 
     print('bis bald')
