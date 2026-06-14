79b1c1b0d98fc3b170555d916672e829a9f8d1a6
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2026-06-14 11:22:53 -0700

    comments

--- dlernen/dlernen_json_schema.py
+++ dlernen/dlernen_json_schema.py
@@ -40,11 +40,13 @@ from referencing import Registry, Resource
 #
 ##########################################################
 
+# must contain at list 1 non-whitespace character
 STRING_PATTERN = r"""\S"""
 
 # for names, leading/trailing whitespace is prohibited.
 NAME_PATTERN = r"""^\S(.*\S)*$"""
 
+# may not contain any whitespace
 WORD_PATTERN = r"""^\S+$"""
 
 ID_PATTERN = WORD_PATTERN
