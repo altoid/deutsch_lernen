a7c83ede9fd2c2184a74e21119f17100f9874e78
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2026-06-15 23:01:40 -0700

    put score data into candidate view

--- sql_worth_keeping/quiz_candidate_v.sql
+++ sql_worth_keeping/quiz_candidate_v.sql
@@ -13,9 +13,17 @@ select
     m.attribute_id,
     m.attrkey,
     m.sort_order,
-    m.attrvalue
+    m.attrvalue,
+    qscore.last_presentation,
+    ifnull(qscore.correct_count, 0) correct_count,
+    ifnull(qscore.presentation_count, 0) presentation_count,
+    ifnull(qscore.correct_count / qscore.presentation_count, 0.0) raw_score
 
 from quiz q
 inner join quiz_structure qs on qs.quiz_id = q.id
 left join mashup_v m on qs.attribute_id = m.attribute_id
+left join quiz_score_v qscore
+    on qscore.quiz_id = qs.quiz_id
+    and qscore.word_id = m.word_id
+    and qscore.attribute_id = m.attribute_id
 ;
