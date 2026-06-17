72fa475025b17e36e4d4c52b45c49b5b519e24dc
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2026-06-17 00:12:43 -0700

    check that selector is valid

--- dlernen/app_quiz.py
+++ dlernen/app_quiz.py
@@ -2,6 +2,7 @@ from flask import Blueprint, url_for
 from pprint import pprint, pformat
 import requests
 import click
+from dlernen import api_quiz_2
 from dlernen.dlernen_json_schema import ATTRIBUTES
 bp = Blueprint('app_quiz', __name__)
 
@@ -25,6 +26,10 @@ def quiz(quiz_key, wordlist_id, selector, tags):
         print("wordlist id is required for now")
         return "OK", 200
 
+    if selector and selector not in api_quiz_2.SELECTORS:
+        print("invalid selector:  %s" % selector)
+        return "OK", 200
+    
     # to run this, use the command:
     #   python -m flask --app run app_quiz quiz <quiz_key> [-l id -l id -l id ...]
     #
