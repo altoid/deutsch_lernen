7afcb0e32f4eff6f05918c63f3bbb37c77f9ab64
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2026-06-27 22:28:40 -0700

    created module json_schema_patterns

--- dlernen/dlernen_json_schema.py
+++ dlernen/dlernen_json_schema.py
@@ -1,5 +1,8 @@
 import jsonschema
 from referencing import Registry, Resource
+from dlernen.json_schema_patterns import STRING_PATTERN, \
+    NAME_PATTERN, WORD_PATTERN, ID_PATTERN, MULTILINE_STRING_PATTERN, ATTRIBUTES
+
 
 #
 #
@@ -33,28 +36,6 @@ from referencing import Registry, Resource
 # - add a test case to tests_schema.py
 #
 #
-
-##########################################################
-#
-#                   Patterns
-#
-##########################################################
-
-# must contain at list 1 non-whitespace character
-STRING_PATTERN = r"""\S"""
-
-# for names, leading/trailing whitespace is prohibited.
-NAME_PATTERN = r"""^\S(.*\S)*$"""
-
-# may not contain any whitespace
-WORD_PATTERN = r"""^\S+$"""
-
-ID_PATTERN = WORD_PATTERN
-
-# separate regex for multiline strings.
-MULTILINE_STRING_PATTERN = r"""\S"""
-
-ATTRIBUTES = 'attributes'
 
 ##########################################################
 #
---
+++ dlernen/json_schema_patterns.py
@@ -0,0 +1,22 @@
+##########################################################
+#
+#                   Patterns
+#
+##########################################################
+
+# must contain at list 1 non-whitespace character
+STRING_PATTERN = r"""\S"""
+
+# for names, leading/trailing whitespace is prohibited.
+NAME_PATTERN = r"""^\S(.*\S)*$"""
+
+# may not contain any whitespace
+WORD_PATTERN = r"""^\S+$"""
+
+ID_PATTERN = WORD_PATTERN
+
+# separate regex for multiline strings.
+MULTILINE_STRING_PATTERN = r"""\S"""
+
+ATTRIBUTES = 'attributes'
+
