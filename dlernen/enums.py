0b86ab2881ea6d7b3e9a8c2728af9fbc971616ec
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2026-07-01 16:00:15 -0700

    added AttrKey enum type

--- dlernen/enums.py
+++ dlernen/enums.py
@@ -53,3 +53,27 @@ def create_posname_class(app):
             # Construct the class safely using EnumType
             return EnumType(class_name, bases, class_dict)
 
+
+def create_attrkey_class(app):
+    class_name = "AttrKey"
+    bases = (StrEnum,)  # Base classes must be inside a tuple
+
+    # 1. Prepare the specialized EnumDict namespace required by EnumType
+    class_dict = EnumType.__prepare__(class_name, bases)
+
+    # 2. Inject values from database as static enum members
+    with app.app_context():
+        with closing(connect(**app.config['DSN'])) as dbh, closing(dbh.cursor(dictionary=True)) as cursor:
+            sql = """
+                select attrkey
+                from attribute
+            """
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            values_dict = {x['attrkey'].replace(' ', '_').upper(): x['attrkey'] for x in rows}
+
+            # note:  class_dict is an _EnumDict, so the |= operator won't work.  but .update() works fine.
+            class_dict.update(values_dict)
+
+            # Construct the class safely using EnumType
+            return EnumType(class_name, bases, class_dict)
--- tests/tests_api_pos.py
+++ tests/tests_api_pos.py
@@ -78,3 +78,37 @@ class APIPosName(unittest.TestCase):
         self.assertTrue('Separable_Prefix' in self.POSName)
 
 
+class APIAttrKey(unittest.TestCase):
+    # make sure dynamically-created POSName Enum class is well-behaved
+
+    app = None
+    app_context = None
+    client = None
+    AttrKey = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = create_app()
+        cls.app.config.update(
+            TESTING=True,
+        )
+
+        cls.AttrKey = cls.app.extensions.get('AttrKey')
+        cls.client = cls.app.test_client()
+        cls.app_context = cls.app.app_context()
+        cls.app_context.push()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.app_context.pop()
+
+    def test1(self):
+        self.assertEqual('article', self.AttrKey.ARTICLE)
+
+    def test2(self):
+        self.assertTrue(self.AttrKey.DEFINITION in self.AttrKey)
+
+    def test3(self):
+        self.assertTrue('past_participle' in self.AttrKey)
+
+
